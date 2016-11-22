6078e66b77863595500c95a786ce818eb6b49449
Uashir <uashir@mail.ru>
2016-11-22 07:02:31 +0300

    Tasks 23-25

--- labs/lib/fork.c
+++ labs/lib/fork.c
@@ -25,6 +25,11 @@ pgfault(struct UTrapframe *utf)
 	//   (see <inc/memlayout.h>).
 
 	// LAB 4: Your code here.
+	if ((err & FEC_WR) == 0)
+		panic("pgfault: non write error, addr = %08x", addr);
+
+	if ((vpt[VPN(addr)] & PTE_COW) == 0)
+		panic("pgfault: write to non-cow page addr = %08x", addr);
 
 	// Allocate a new page, map it at a temporary location (PFTEMP),
 	// copy the data from the old page to the new page, then move the new
@@ -34,8 +39,18 @@ pgfault(struct UTrapframe *utf)
 	//   No need to explicitly delete the old page's mapping.
 
 	// LAB 4: Your code here.
+	r = sys_page_alloc(0, PFTEMP, PTE_U|PTE_P|PTE_W);
+	if (r < 0)
+		panic("sys_page_alloc failed: %e", r);
 
-	panic("pgfault not implemented");
+	memmove(PFTEMP, ROUNDDOWN(addr, PGSIZE), PGSIZE);
+	r = sys_page_map(0, PFTEMP, 0, ROUNDDOWN(addr, PGSIZE), PTE_U|PTE_P|PTE_W);
+	if (r < 0)
+		panic("sys_page_map failed: %e", r);
+
+	r = sys_page_unmap(0, PFTEMP);
+	if (r < 0)
+		panic("sys_page_unmap failed: %e", r);
 }
 
 //
@@ -55,7 +70,22 @@ duppage(envid_t envid, unsigned pn)
 	int r;
 
 	// LAB 4: Your code here.
-	panic("duppage not implemented");
+	void *va = (void *)(pn * PGSIZE);
+	pte_t pte = vpt[pn];
+
+	if ((pte & PTE_W) != 0 || (pte & PTE_COW) != 0) {
+		r = sys_page_map(0, va, envid, va, PTE_P|PTE_U|PTE_COW);
+		if (r < 0)
+			return r;
+
+		r = sys_page_map(0, va, 0, va, PTE_P|PTE_U|PTE_COW);
+		if (r < 0)
+			return r;
+	} else {
+		r = sys_page_map(0, va, envid, va, PTE_U|PTE_P);
+		if (r < 0)
+			return r;
+	}
 	return 0;
 }
 
@@ -79,7 +109,49 @@ envid_t
 fork(void)
 {
 	// LAB 4: Your code here.
-	panic("fork not implemented");
+	set_pgfault_handler(pgfault);
+
+	envid_t child_id = sys_exofork();
+	if (child_id < 0)
+		panic("sys_exofork failed: %e", child_id);
+
+	if (child_id == 0) {
+		env = &envs[ENVX(sys_getenvid())];
+		return 0;
+	}
+
+	extern unsigned char end[];
+	int pdi;
+	for (pdi = VPD(0); pdi <= VPD(end); pdi++) {
+		if ((vpd[pdi] & PTE_P) == 0)
+			continue;
+
+		int pti;
+		for (pti = pdi * NPDENTRIES; pti < pdi * NPDENTRIES + NPTENTRIES; pti++) {
+			if ((vpt[pti] & PTE_P) == 0)
+				continue;
+
+			int r = duppage(child_id, pti);
+			if (r < 0)
+				panic("duppage failed: %e", r);
+		}
+	}
+
+    duppage(child_id, PPN(&child_id));
+
+	int r = sys_page_alloc(child_id, (void *)(UXSTACKTOP - PGSIZE), PTE_P|PTE_U|PTE_W);
+	if (r < 0)
+		panic("sys_page_alloc failed: %e\n", r);
+
+	r = sys_env_set_pgfault_upcall(child_id, env->env_pgfault_upcall);
+	if (r < 0)
+		panic("sys_env_set_pgfault_upcall failed: %e", r);
+
+	// Start the child environment running
+	if ((r = sys_env_set_status(child_id, ENV_RUNNABLE)) < 0)
+		panic("sys_env_set_status failed: %e", r);
+
+	return child_id;
 }
 
 // Challenge!
